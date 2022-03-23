124d077af0b1b66a936c04669715029b0e9b3a35
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-03-23 10:18:33 -0700

    add code to script for input of form

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
-  
+  ("#formOne").submit(function(event) {
+    event.preventDefault();
+    const userNameInput = $("input#userName").val();
+    const petsName = $("input#petsName").val();
+    const summerActivities = $("input#summerActivites").val();
+  });
 
 
-  
+
 })
