6ef9dbafd5936e34facb649fdb838641e359b6f5
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-03-23 10:33:37 -0700

    fix the script to show the paragraph properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
-  ("#formOne").submit(function(event) {
+  $("#formOne").submit(function(event) {
     event.preventDefault();
     const userNameInput = $("input#userName").val();
     const petsName = $("input#petsName").val();
     const summerActivities = $("input#summerActivites").val();
+
+    $(".userName").text(userNameInput);
+    $(".petsName").text(petsName);
+    $(".summerActivities").text(summerActivities);
+
+    $("#letter-received").show();
   });
-
-
-
 })
